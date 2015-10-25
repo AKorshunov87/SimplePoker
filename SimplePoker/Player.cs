de7683e389524bf3e99b85ed2061a15abfb16364
AKorshunov87 <a.korshunov@gmail.com>
2015-10-25 18:19:00 +0300

    Player class implemented
    Player class and tests implemented

--- SimplePoker/Player.cs
+++ SimplePoker/Player.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace SimplePoker {
+    /// <summary>
+    /// Information about player
+    /// </summary>
+    public class Player {
+        string name;
+
+        public string Name { get { return name; } }
+
+        public Player() {
+            name = "Anonymous";
+        }
+        public Player(string name) : this() {
+            this.name = name;
+        }
+    }
+}
---
+++ SimplePokerTests/PlayerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using SimplePoker;
+
+namespace SimplePokerTests {
+    [TestFixture]
+    public class PlayerTests {
+        void PlayerValuesTest(Player player, string name) {
+            Assert.AreEqual(name, player.Name);
+        }
+
+        [Test]
+        public void PlayerValues() {
+            Player defaultPlayer = new Player();
+            PlayerValuesTest(defaultPlayer, "Anonymous");
+            Player player = new Player("John");
+            PlayerValuesTest(player, "John");
+        }
+    }
+}
