5a778798018a9e23753d387baf72ddd3d7e341f9
AKorshunov87 <a.korshunov87@gmail.com>
2015-10-27 07:31:08 +0300

    Subset interface
    Subset interface added, and subsetType enum

---
+++ SimplePoker/Subsets/ISubset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SimplePoker.Subsets {
+    /// <summary>
+    /// Interface for subsets
+    /// </summary>
+    internal interface ISubset {
+        /// <summary>
+        /// Type of subset
+        /// </summary>
+        SubsetType SubsetType { get; }
+        /// <summary>
+        /// Get subsets from the cards set(hand) of the current player
+        /// </summary>
+        /// <param name="cards">cards of the player</param>
+        /// <returns>List of subsets, each exists its own cards</returns>
+        List<List<Card>> GetSubsets(Card[] cards);
+    }
+}
---
+++ SimplePoker/Subsets/SubsetType.cs
@@ -0,0 +1,12 @@
+﻿namespace SimplePoker.Subsets {
+    /// <summary>
+    /// Subset types ( Flush, Four of a Kind, etc)
+    /// </summary>
+    internal enum SubsetType {
+        HighCard = 1,
+        TwoOfAKind = 2,
+        ThreeOfAKind = 3,
+        FourOfAKind = 4,
+        Flush = 5
+    }
+}
